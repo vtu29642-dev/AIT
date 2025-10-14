eafbfb4f9a9a1dc04a38955221df1b7c01ab174b
vtu29642-dev <vtu29642@veltech.edu.in>
2025-10-14 11:12:33 +0530

    Add files via upload

---
+++ task3.py
@@ -0,0 +1,13 @@
+graph=[[0,0,0],
+       [0,1,0],[0,0,0]]
+print("create a graph ")
+for i in graph:
+    for j in i:
+        print (j, end= " ")
+    print()
+print ("paths")
+path=[(0,0),(0,1),(0,2),(1,0),(1,1),(1,2),(2,0),(2,1),(2,2)]
+for i in path:
+    print(i)
+
+ 
